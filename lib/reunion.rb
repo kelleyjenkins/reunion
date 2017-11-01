e67a876020a9c35766b165ea0f83942b9b5b4fd7
kelleyjenkins <ckjenkins3@gmail.com>
2017-11-01 09:36:03 -0600

    Add reunion tests

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,2 +1,17 @@
 class Reunion
+  attr_reader :activities
+
+  def initialize(location)
+    @location = location
+    @activities = []
+  end
+
+  def add_activity(activity)
+    @activities << activity
+  end
+
+  def total_cost
+    
+  end
+
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -1,7 +1,47 @@
 require 'minitest'
 require 'minitest/test'
 require 'minitest/autorun'
+require 'minitest/pride'
 require './lib/reunion'
+require './lib/activity'
 
 class ReunionTest < Minitest::Test
+
+  def test_it_existis
+    reunion = Reunion.new("Denver")
+
+    assert_instance_of Reunion, reunion
+  end
+
+  def test_it_has_activities
+    reunion = Reunion.new("Denver")
+
+    assert_equal [], reunion.activities
+  end
+
+  def test_it_can_add_activies_to_reunion
+    reunion = Reunion.new("Denver")
+    activity = Activity.new("dinner")
+
+    reunion.add_activity(activity)
+
+    assert_equal 1, reunion.activities.count
+    assert_equal "dinner", reunion.activities[0].name
+  end
+
+  def test_it_can_add_2_activities_to_reunion
+    reunion = Reunion.new("Denver")
+    activity = Activity.new("dinner")
+    activity2 = Activity.new("ice skating")
+
+    reunion.add_activity(activity)
+    reunion.add_activity(activity2)
+
+
+    assert_equal 2, reunion.activities.count
+    assert_equal "ice skating", reunion.activities[1].name
+  end
+
+  
+
 end
