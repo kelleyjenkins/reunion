055df9db43d7e7fb4816e78ddf0e7203903b41d5
kelleyjenkins <ckjenkins3@gmail.com>
2017-11-01 09:14:40 -0600

    Add more tests

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,8 +1,15 @@
 class Activity
-  attr_reader :name
+  attr_reader :name,
+              :participants
 
   def initialize(name)
     @name = name
+    @participants = {}
   end
+
+  def add_participant(person, cost)
+    participants[person] = cost
+  end
+
 
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -16,5 +16,20 @@ class ActivityTest < Minitest::Test
 
     assert_equal "hiking", activity.name
   end
-  
+
+  def test_it_has_participants_hash
+    activity = Activity.new("hiking")
+
+
+    assert activity.participants
+    assert_instance_of Hash, activity.participants
+  end
+
+
+  def test_it_can_add_one_participant
+    activity = Activity.new("hiking")
+    activity.add_participant("Sarah", 10)
+
+    assert_equal 1, activity.participants.count
+  end
 end
